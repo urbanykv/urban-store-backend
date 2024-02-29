e623d228fbadb8f788b21d9d1817c4a8f482707c
Matheus Urban <133094521+urbanykv@users.noreply.github.com>
2024-02-29 17:00:44 -0300

    adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { router } from './routes';
-import { connect } from './connection/connection'; 
+import { connect } from './connection/connection';
+import cors from 'cors';
 import { config } from 'dotenv';
 config();
 
 const app = express();
+
+app.use(cors);
 
 app.use(express.json());
 
@@ -15,5 +18,3 @@ connect();
 const port = process.env.PORT;
 
 app.listen(port, () => console.log('server on'));
-
-console.log(port);
