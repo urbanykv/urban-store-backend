e9f1b948f8920d0d6716d6116af037a30e429b12
Matheus Urban <133094521+urbanykv@users.noreply.github.com>
2024-02-29 17:17:46 -0300

    build

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
-app.use(cors_1.default);
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(routes_1.router);
 (0, connection_1.connect)();
