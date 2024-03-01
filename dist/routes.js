27dea0217d0795c51b427f45ceaa4f3a87fc4e4c
Matheus Urban <133094521+urbanykv@users.noreply.github.com>
2024-03-01 18:18:04 -0300

    build

---
+++ dist/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const productController_1 = require("./controllers/productController");
+const productController = new productController_1.ProductController();
+exports.router = (0, express_1.Router)();
+exports.router.get('/produtos', productController.getAllProducts);
+exports.router.get('/produtos/:id', productController.getProductByID);
