920a98ded86a6b1199ac91242d42f0e3decd0fd3
urbanykv <162392016+urbanelystech@users.noreply.github.com>
2024-03-10 23:13:55 -0300

    testando vercel

--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const productController_1 = require("./controllers/productController");
-const productController = new productController_1.ProductController();
-exports.router = (0, express_1.Router)();
-exports.router.get('/produtos', productController.getAllProducts);
-exports.router.get('/produtos/:id', productController.getProductByID);
